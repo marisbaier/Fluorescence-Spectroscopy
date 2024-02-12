592b0dcb022d8e209cde776375ba70336fa6da83
Maris Baier <maris.baier@physik.hu-berlin.de>
2024-02-12 15:24:56 +0100

    Mkdir scoring/, Rename scoring.py

--- config.py
+++ config.py
@@ -22,6 +22,7 @@ if not os.path.exists(path):
     os.makedirs(path + 'Autoencoder/weights')
     os.makedirs(path + 'FC/weights')
     os.makedirs(path + 'FC/FC_dataset/')
+    os.makedirs(path + 'scoring')
     print(f"Directory '{path}' created.")
 else:
     print(f"Directory '{path}' already exists. (check)")
